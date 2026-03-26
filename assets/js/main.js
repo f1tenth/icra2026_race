57fc8fe7774c72280014432ecacfbb54c5227535
Jaroslav Klapálek <jaroslav.klapalek@tuwien.ac.at>
2026-03-26 10:29:10 +0100

    Increase the 'medium' page size to 1000px
    This improves the UX on the screens 980-1000px as earlier the navbar
    was not rendered properly (labels are too long).

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,8 +18,8 @@
 		breakpoints({
 			default:   ['1681px',   null       ],
 			xlarge:    ['1281px',   '1680px'   ],
-			large:     ['981px',    '1280px'   ],
-			medium:    ['737px',    '980px'    ],
+			large:     ['1001px',    '1280px'  ],
+			medium:    ['737px',    '1000px'   ],
 			small:     ['481px',    '736px'    ],
 			xsmall:    ['361px',    '480px'    ],
 			xxsmall:   [null,       '360px'    ]
